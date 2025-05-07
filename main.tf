2c68fb73f3e81289bd9b880f6ab5f95b2aa656ea
pratheesh-dev-tech <iq0pooki@gmail.com>
2025-05-08 02:43:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,25 @@
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "rathidevisuresh"
-  acl    = "private"
+provider "aws" {
+  region = "us-west-2"
 }
 
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-example-bucket"
+
+  tags = {
+    Name        = "MyExampleBucket"
+    Environment = "Dev"
+  }
+}
+
+# Define ACL separately using aws_s3_bucket_acl resource
 resource "aws_s3_bucket_acl" "example_bucket_acl" {
   bucket = aws_s3_bucket.example_bucket.bucket
   acl    = "private"
 }
 
-resource "aws_s3_object" "example_object" {
-  bucket  = aws_s3_bucket.example_bucket.bucket
-  key     = "example.txt"
-  content = "This is an example object."
-  acl     = "private"
+resource "aws_s3_bucket_object" "example_object" {
+  bucket = aws_s3_bucket.example_bucket.bucket
+  key    = "example-object"
+  source = "path/to/local/file"
+  acl    = "private"
 }
